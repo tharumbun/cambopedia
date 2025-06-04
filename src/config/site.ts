db034aff4e3a9f2f4e54777181c532999c3b7d30
kokitree <94977757+tharumbun@users.noreply.github.com>
2025-06-04 10:33:55 +0700

    updated

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   // url is removed, Astro.site will be used
 
   // from config.json:site (logo related)
-  logo: "/logo.svg",
+  logo: "/logo.png",
   logoWidth: "200",
   logoHeight: "30",
   logoText: "Cambopedia",
